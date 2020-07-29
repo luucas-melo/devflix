53195d0d3afbc898607bf0357f9ed83aaa86e9df
luucasxd <lucasmelodos322@gmail.com>
2020-07-29 16:03:03 -0300

    mudando o estado no formulário

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,52 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
-import { Label ,Button } from './styles';
+import { Label, Button } from './styles';
 function CadastroCategoria() {
+  const [categorias, setCategorias] = useState(['Teste']);
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');
+
   return (
     <PageDefault>
 
-      <h1 className="title">Nova categoria</h1>
-      <form>
-        <Label>
-          <input
-            type="text"
-            placeholder="Nome"
-          />
-        </Label>
-        <Label>
-          <textarea
-            type="text"
-            placeholder="Teste"
-          />
-        </Label>
-        <Label>
-          <input
-            type="text"
-            placeholder="Cor"
-          />
-        </Label>
-        <Label>
+      <h1 >Cadastro de Categoria: {nomeDaCategoria}</h1>
+      <form  onSubmit={function handleSubmit(event) {
+        event.preventDefault()
+        setCategorias([
+          ...categorias,
+          nomeDaCategoria
+        ]);
+      }}>
+        <div>
+          <label>
+            <input
+              type="text"
+              placeholder="Nome"
+              value={nomeDaCategoria}
+              onChange={function handleOnCHange(info) {
+                setNomeDaCategoria(info.target.value)
+              }}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <textarea
+              type="text"
+              placeholder="Teste"
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <input
+              type="color"
+              placeholder="Cor"
+            />
+          </label>
+        </div>
+
+        <div>
+        <label>
           <input
             type="text"
             placeholder="Código de segurança"
           />
-        </Label>
+        </label>
+        </div>
+
+        <div>
         <Button>
           Salvar
         </Button>
         <Button>
           Limpar
         </Button>
-
+        </div>
       </form>
-
-
-      <Link to="/">
-        Ir para home
+    <ul>
+      {categorias.map((categoria, index) => {
+        return (
+          <li key={`${categoria}${index}`}>
+            {categoria}
+          </li>
+        );
+      })}
+    </ul>
+    <Link to="/">
+      Ir para home
       </Link>
 
-    </PageDefault>
+    </PageDefault >
   )
 }
 
