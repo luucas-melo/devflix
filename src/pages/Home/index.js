a1374987a57558097397d7ee1bf1f8903d663285
luucasxd <lucasmelodos322@gmail.com>
2020-07-31 12:54:20 -0300

    colocando imagem na home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Menu from '../../components/Menu';
-import dadosIniciais from '../../data/dados_iniciais.json';
+import initialData from '../../data/initial_data.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import Footer from '../../components/Footer';
@@ -11,30 +11,30 @@ function Home() {
       <Menu />
 
       <BannerMain
-        videoTitle={dadosIniciais.categories[0].videos[0].title}
-        url={dadosIniciais.categories[0].videos[0].url}
-        videoDescription="Acompanhe essa série sobre matemática básica, afinal, uma boa base possibilita construir um bom aprendizado."
+        videoTitle={initialData.categories[0].videos[0].title}
+        url={initialData.categories[0].videos[0].url}
+        videoDescription="Aprenda de uma maneira simples a fazer um bom ravioli com a vencedora do mastercheff profisional, Deise Paparoto."
       />
 
       <Carousel
         ignoreFirstVideo
-        category={dadosIniciais.categories[0]}
+        category={initialData.categories[0]}
       />
 
       <Carousel
-        category={dadosIniciais.categories[1]}
+        category={initialData.categories[1]}
       />
 
       <Carousel
-        category={dadosIniciais.categories[2]}
+        category={initialData.categories[2]}
       />
 
       <Carousel
-        category={dadosIniciais.categories[3]}
+        category={initialData.categories[3]}
       />
 
       <Carousel
-        category={dadosIniciais.categories[4]}
+        category={initialData.categories[4]}
       />
 
       <Footer />
